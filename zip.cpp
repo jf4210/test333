03753c42cfaefe608649f8b9f5442c821c09b08a
github <jf4210@yeah.net>
2016-11-24 17:48:51 +0800

    f

--- zip.cpp
+++ zip.cpp
@@ -2582,7 +2582,8 @@ ZRESULT TZip::Add(const TCHAR *odstzn, void *src,unsigned int len, DWORD flags)
   TZipFileInfo zfi; zfi.nxt=NULL;
   strcpy_s(zfi.name,"");
 #ifdef UNICODE
-  WideCharToMultiByte(CP_UTF8,0,dstzn,-1,zfi.iname,MAX_PATH,0,0);
+//  WideCharToMultiByte(CP_UTF8,0,dstzn,-1,zfi.iname,MAX_PATH,0,0);
+  WideCharToMultiByte(CP_ACP,0,dstzn,-1,zfi.iname,MAX_PATH,0,0);
 #else
   strcpy_s(zfi.iname,dstzn);
 #endif
